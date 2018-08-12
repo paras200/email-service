0115c19f5a48815c6440a31f5723baa027dfadaf
paras agnihotri <pawarbhu0310@gmail.com>
2018-08-12 14:36:19 +0530

    changes in template

--- src/main/java/com/myBuddy/email/controller/EmailController.java
+++ src/main/java/com/myBuddy/email/controller/EmailController.java
@@ -19,7 +19,7 @@ import com.myBuddy.email.model.EmailDetails;
 
 @RestController
 @CrossOrigin(origins = "*")
-//@RequestMapping(path="/email")
+// @RequestMapping(path="/email")
 public class EmailController {
 	@Autowired
 	private JavaMailSender sender;
@@ -30,13 +30,15 @@ public class EmailController {
 		MimeMessageHelper helper = new MimeMessageHelper(message, false, "utf-8");
 		List<String> emailTo = new ArrayList<>();
 		emailTo.add("agnihotri.paras@gmail.com");
-		//emailTo.add("sinhanil19@gmail.com");
-		//emailTo.add("er.mr.bhushan@gmail.com");
+		emailTo.add("sinhanil19@gmail.com");
+		emailTo.add("er.mr.bhushan@gmail.com");
 		try {
 			helper.setTo(emailTo.toArray(new String[emailTo.size()]));
 			helper.setFrom("My-Buddy");
 			helper.setSubject("My Buddy Email Service");
-			helper.setText("<p>Dear Sir or Madam:<br></p><p><br>I have recently ordered a new pair of soccer cleats (item #6542951) from your website on June 21. I received the order on June 26. Unfortunately, when I opened it, I saw that the cleats were used. The cleats had dirt all over it and there was a small tear in front of the part where the left toe would go.  My order number is AF26168156.<br></p><p><br>To resolve the problem, I would like you to credit my account for the amount charged for my cleats; I have already went out and bought a new pair of cleats at my local sporting goods store so sending another would result in me having two pairs of the same cleats.<br></p><p><br>Than you for taking the time to read this letter.  I have been a satisfied customer of your company for many years and this is the first time I have encountered a problem.  If you need to contact me, you can reach me at (555) 555-5555.<br><br><br><br>Sincerely,<br></p><p><br><i>Signature</i></p>", true);
+			helper.setText(
+					"<p>Dear Sir or Madam:<br></p><p><br>I have recently ordered a new pair of soccer cleats (item #6542951) from your website on June 21. I received the order on June 26. Unfortunately, when I opened it, I saw that the cleats were used. The cleats had dirt all over it and there was a small tear in front of the part where the left toe would go.  My order number is AF26168156.<br></p><p><br>To resolve the problem, I would like you to credit my account for the amount charged for my cleats; I have already went out and bought a new pair of cleats at my local sporting goods store so sending another would result in me having two pairs of the same cleats.<br></p><p><br>Than you for taking the time to read this letter.  I have been a satisfied customer of your company for many years and this is the first time I have encountered a problem.  If you need to contact me, you can reach me at (555) 555-5555.<br><br><br><br>Sincerely,<br></p><p><br><i>Signature</i></p>",
+					true);
 		} catch (MessagingException e) {
 			e.printStackTrace();
 			return "Error while sending mail ..";
@@ -47,55 +49,121 @@ public class EmailController {
 
 	@PostMapping("/sendTemplateMail")
 	public String sendMailfromTemplate(@RequestBody EmailDetails details) throws MessagingException {
-		if(details.template==null || details.template.trim().equals("")) return "Template name not specified";
+		if (details.template == null || details.template.trim().equals(""))
+			return "Template name not specified";
 		MimeMessage message = sender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message, false, "utf-8");
 		try {
 			if (details.template.equalsIgnoreCase("new-project-added")) {
-				if(details.paramMap == null || details.paramMap.isEmpty()) return "Parameters not specified"; 
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
 				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
-				helper.setText("<p>Hello,<br></p><p><br>New project has been created "+details.paramMap.get("projectName")+" Please enroll to start working on this project. <br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
+				helper.setText("<p>Hello,<br></p><p><br>New project has been created "
+						+ details.paramMap.get("projectName")
+						+ " Please enroll to start working on this project. <br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>",
+						true);
 				helper.setSubject("New Projec created - enrollment open !!");
-			}else if (details.template.equalsIgnoreCase("payment-done")) {
-				if(details.paramMap == null || details.paramMap.isEmpty()) return "Parameters not specified"; 
+
+			} else if (details.template.equalsIgnoreCase("payment-done")) {
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
 				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
-				helper.setText("<p>Hello,<br></p><p><br>Amount - "+details.paramMap.get("amount")+" has been transferred from user - "+details.paramMap.get("fromUser")+"  to user - "+details.paramMap.get("toUser")+"<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
-				//helper.setText("<p>Dear Sir or Madam:<br></p><p><br>I have recently ordered a new pair of soccer cleats (item #6542951) from your website on June 21. I received the order on June 26. Unfortunately, when I opened it, I saw that the cleats were used. The cleats had dirt all over it and there was a small tear in front of the part where the left toe would go.  My order number is AF26168156.<br></p><p><br>To resolve the problem, I would like you to credit my account for the amount charged for my cleats; I have already went out and bought a new pair of cleats at my local sporting goods store so sending another would result in me having two pairs of the same cleats.<br></p><p><br>Than you for taking the time to read this letter.  I have been a satisfied customer of your company for many years and this is the first time I have encountered a problem.  If you need to contact me, you can reach me at (555) 555-5555.<br><br><br><br>Sincerely,<br></p><p><br><i>Signature</i></p>", true);
+				helper.setText("<p>Hello,<br></p><p><br>" + details.paramMap.get("credit")
+						+ " Credits has been transferred from user - " + details.paramMap.get("fromUser")
+						+ "  to user - " + details.paramMap.get("toUser")
+						+ "<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
 				helper.setSubject("Payment done successfully");
-			}else if (details.template.equalsIgnoreCase("add-credit")) {
-				if(details.paramMap == null || details.paramMap.isEmpty()) return "Parameters not specified"; 
+
+			} else if (details.template.equalsIgnoreCase("payment-failed")) {
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
 				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
-				helper.setText("<p>Hello,<br></p><p><br>"+details.paramMap.get("credit")+" added to user with email - "+details.paramMap.get("email")+", same will reflect in your home page in the Credit section<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
+				helper.setText("<p>Hello,<br></p><p><br>Payment of " + details.paramMap.get("amount")
+						+ "  "+details.paramMap.get("cry")+" has failed - "
+						+ " Please retry the transaction <br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
+				helper.setSubject("Payment Failed");
+
+			} else if (details.template.equalsIgnoreCase("add-credit")) {
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
+				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
+				helper.setText("<p>Hello,<br></p><p><br>" + details.paramMap.get("credit")
+						+ " added to user with email - " + details.paramMap.get("email")
+						+ ", same will reflect in your home page in the Credit section<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>",
+						true);
 				helper.setSubject("Credits added to your profile");
-			}else if (details.template.equalsIgnoreCase("withdraw-credit")) {
-				if(details.paramMap == null || details.paramMap.isEmpty()) return "Parameters not specified"; 
+
+			} else if (details.template.equalsIgnoreCase("withdraw-credit")) {
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
 				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
-				helper.setText("<p>Hello,<br></p><p><br>Request recieved for withdrawal of "+details.paramMap.get("credit")+" creditds for you account linked with email -  "+details.paramMap.get("skills")+". We will process this request within 5-7 business working days and the amount will be credited to your account. Please write to us in case of any doubts.<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
+				helper.setText("<p>Hello,<br></p><p><br>Request recieved for withdrawal of "
+						+ details.paramMap.get("amount") + "  " + details.paramMap.get("ccy")
+						+ " which is equivalnet of " + details.paramMap.get("credit")
+						+ " for you account linked with email -  " + details.paramMap.get("email")
+						+ "  . We will process this request within 5-7 business working days and the amount will be credited to your account. Please write to us in case of any doubts.<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>",
+						true);
 				helper.setSubject("Withdraw credit request - Successful");
-			}else if (details.template.equalsIgnoreCase("txn-failed")) {
-				if(details.paramMap == null || details.paramMap.isEmpty()) return "Parameters not specified"; 
+
+			} else if (details.template.equalsIgnoreCase("txn-success")) {
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
 				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
-				helper.setText("<p>Hello,<br></p><p><br>Your transaction to "+details.paramMap.get("txntype")+" For "+details.paramMap.get("credit")+" credits has failed, please retry the transaction. <br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
+				helper.setText("<p>Hello,<br></p><p><br>" + details.paramMap.get("credit")
+						+ " Credits has been transferred from user - " + details.paramMap.get("fromUser")
+						+ "  to user - " + details.paramMap.get("toUser")
+						+ ". A transaction charge of "+details.paramMap.get("txnCharge")+" has beein charged<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
+				helper.setSubject("Transaction successfull");
+
+			} else if (details.template.equalsIgnoreCase("txn-failed")) {
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
+				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
+				helper.setText("<p>Hello,<br></p><p><br>Your transaction to transfer funds to "
+						+ details.paramMap.get("toUser") + " For " + details.paramMap.get("nosOfcredit")
+						+ " credits has failed, please retry the transaction. <br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>",
+						true);
 				helper.setSubject("Transaction faliure");
-			}else if (details.template.equalsIgnoreCase("new-user")) {
-				if(details.paramMap == null || details.paramMap.isEmpty()) return "Parameters not specified"; 
+
+			} else if (details.template.equalsIgnoreCase("add-credit-failed")) {
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
 				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
-				helper.setText("<p>Hello,<br></p><p><br>New user with email - "+details.paramMap.get("email")+" with skills as "+details.paramMap.get("skills")+" has been added. <br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
+				helper.setText("<p>Hello,<br></p><p><br>Your transaction to add " + details.paramMap.get("credit")
+						+ " credits has failed, please retry the transaction or get in touch with us using our contact us page. <br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>",
+						true);
+				helper.setSubject("Credit addition faliure ");
+
+			} else if (details.template.equalsIgnoreCase("new-user")) {
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
+				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
+				helper.setText("<p>Hello,<br></p><p><br>New user with email - " + details.paramMap.get("email")
+						+ " with skills as " + details.paramMap.get("skills")
+						+ " has been added. <br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>",
+						true);
 				helper.setSubject("Welcome to My Buddy");
-			}else if (details.template.equalsIgnoreCase("new-enrollment-po")) {
-				if(details.paramMap == null || details.paramMap.isEmpty()) return "Parameters not specified"; 
+
+			} else if (details.template.equalsIgnoreCase("new-enrollment-po")) {
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
 				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
-				helper.setText("<p>Hello,<br></p><p><br>New Enrollement - "+details.paramMap.get("email")+" with skills as "+details.paramMap.get("skills")+"<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
+				helper.setText("<p>Hello,<br></p><p><br>New Enrollement - " + details.paramMap.get("email")
+						+ " with skills as " + details.paramMap.get("skills")
+						+ "<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
 				helper.setSubject("New enrollment");
-			}else if (details.template.equalsIgnoreCase("new-enrollment-buddy")) {
-				if(details.paramMap == null || details.paramMap.isEmpty()) return "Parameters not specified"; 
+
+			} else if (details.template.equalsIgnoreCase("new-enrollment-buddy")) {
+				if (details.paramMap == null || details.paramMap.isEmpty())
+					return "Parameters not specified";
 				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
-				helper.setText("<p>Hello,<br></p><p><br>New Enrollement - "+details.paramMap.get("email")+" with skills as "+details.paramMap.get("skills")+"<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
+				helper.setText("<p>Hello,<br></p><p><br>New Enrollement - " + details.paramMap.get("email")
+						+ " with skills as " + details.paramMap.get("skills")
+						+ "<br></p><br><br><br><br>Sincerely,<br></p><p><br><i>My Buddy Team</i></p>", true);
 				helper.setSubject("New enrollment");
-			}else {
-				helper.setTo(details.toList.toArray(new String[details.toList.size()]));
-				helper.setText("Oceantree");
-				helper.setSubject("This content is as per the template  -   " + details.template);
+
+			} else {
+				return "Not a recognized template";
 			}
 		} catch (MessagingException e) {
 			e.printStackTrace();
